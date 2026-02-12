a2c66e08401d2ef6b9a066dc1718e2cf59f63b32
migneh <150557976+migneh@users.noreply.github.com>
2026-02-12 20:12:52 +0100

    Add emojis module with various emoji exports

---
+++ emojis.js
@@ -0,0 +1,13 @@
+module.exports = {
+  success: "<a:success:1463617337126293555>",
+  error: "<a:logoa_error:1463617467296256164>",
+  add: "<:8692addgreen:1463619531137093888>",
+  remove: "<:6684removered:1463619534081753250>",
+  top: "<:leaderboard:1463618089424912517>",
+  left: "⬅",
+  right: "➡",
+  reset: "♻️",
+  warning: "⚠️",
+  settings: "⚙️",
+  points: "<a:coin:1463619689866592401>"
+};
